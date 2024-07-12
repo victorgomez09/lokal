61c7a8dfd052e196fea3a37744a7a31780670915
Andreas Ekström <didair@msn.com>
2024-07-12 15:22:48 +0200

    Update dockerfile and startup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Run Prisma migrations
-echo "Waiting 5s before running prisma migrate..."
-
-sleep 5s && npx prisma migrate deploy
-
-echo "Migrate script done"
+# Run Prisma migrations if the environment variable is set
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  echo "Build next.js application"
+  npm run build
+  echo "Waiting 5s before running prisma migrate..."
+  sleep 5s && npx prisma migrate deploy
+  echo "Migrate script done"
+else
+  echo "Skipping Prisma migrations & build"
+fi
 
 # Start the application
 exec "$@"
