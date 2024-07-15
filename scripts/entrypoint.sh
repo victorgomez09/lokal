a1b72c11ecd80d76e236443a5eebf5b96db10617
Andreas Ekström <didair@msn.com>
2024-07-15 10:16:50 +0200

    Update build order in entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,11 +2,11 @@
 
 # Run Prisma migrations if the environment variable is set
 if [ "$RUN_MIGRATIONS" = "true" ]; then
-  echo "Build next.js application"
-  npm run build
   echo "Waiting 5s before running prisma migrate..."
   sleep 5s && npx prisma migrate deploy
   echo "Migrate script done"
+  echo "Build next.js application"
+  npm run build
 else
   echo "Skipping Prisma migrations & build"
 fi
