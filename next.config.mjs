c527a4332b15be9a7dd2ca0e82fa7a890c295078
Andreas Ekström <didair@msn.com>
2024-07-15 10:10:16 +0200

    Update tsconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
-const dirToIgnore = /data/;
-
 const nextConfig = {
-	webpack: (config) => {
-		config.module.rules = config.module.rules.map((rule) => {
-			if (rule !== "..." && rulesToProcess.indexOf(String(rule.test)) > -1) {
-				rule.exclude = [dirToIgnore];
-			}
-			return rule;
-		});
-		return config;
-	},
 };
 
 export default nextConfig;
