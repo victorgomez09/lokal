ca687540930676711b4d0a48909d2235f5c49e53
Andreas Ekström <didair@msn.com>
2024-07-15 09:42:25 +0200

    More ignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
+const dirToIgnore = /data/;
+
 const nextConfig = {
+	webpack: (config) => {
+		config.module.rules = config.module.rules.map((rule) => {
+			if (rule !== "..." && rulesToProcess.indexOf(String(rule.test)) > -1) {
+				rule.exclude = [dirToIgnore];
+			}
+			return rule;
+		});
+		return config;
+	},
 };
 
 export default nextConfig;
