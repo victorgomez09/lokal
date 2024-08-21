1fdb019acbbd77a425b5b86f01fb90eac894aa14
Andreas Ekström <didair@msn.com>
2024-08-21 19:19:10 +0200

    Add data ignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+import webpack from 'webpack';
+
 const nextConfig = {
+	webpack: (config, { isServer }) => {
+		config.plugins.push(
+			new webpack.IgnorePlugin({
+				resourceRegExp: /^data\//, // Ignore all files under the 'data' directory
+			})
+		);
+		return config;
+	},
 };
 
 export default nextConfig;
