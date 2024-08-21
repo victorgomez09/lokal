27df1efab6f9fb7ed2f2aa6d36d6012dd8481d9a
Andreas Ekström <didair@msn.com>
2024-08-21 18:57:13 +0200

    Include setup in middleware rules

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,24 @@
 import { getCurrentSession } from "@/lib/user";
 import { NextRequest, NextResponse } from "next/server";
 
+const unprotectedRoutes = [
+	'/login',
+	'/register',
+	'/setup'
+];
+
 export async function middleware(request: NextRequest) {
 	const session = await getCurrentSession();
 	const { pathname } = request.nextUrl;
 
 	if (session != null) {
-		if (pathname == '/login' || pathname == '/register') {
+		if (unprotectedRoutes.indexOf(pathname) > -1) {
 			return NextResponse.redirect(new URL('/', request.url));
 		}
 	}
 
 	if (session == null) {
-		if (pathname != '/login' && pathname != '/register' && pathname != '/logout') {
+		if (unprotectedRoutes.indexOf(pathname) == -1) {
 			return NextResponse.redirect(new URL('/login', request.url));
 		}
 	}
